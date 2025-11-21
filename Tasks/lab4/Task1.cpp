b495649ddfb29fb8c7475057980e12f293ae7ee1
alitvinov006-create <alitvinov006@gmail.com>
2025-11-21 18:07:56 +0300

    Add files via upload

---
+++ Tasks/lab4/Task1.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    const int k = 5;
+  
+    int A[k];
+    int B[k];
+
+    cout << "Enter elements for array A: " << endl;
+    for (int i = 0; i < k; i++) {
+        cin >> A[i];
+    }
+
+    cout << "Enter elements for array B: " << endl;;
+    for (int i = 0; i < k; i++) {
+        cin >> B[i];
+    }
+
+    int countA = 0;
+    int countB = 0;
+
+    for (int i = 0; i < k; i++) {
+        if (A[i] % 2 == 0) {
+            countA += 1;
+        }
+        if (B[i] % 2 == 0) {
+            countB += 1;
+        }
+    }
+    if (countA >= countB) {
+        for (int i = 0; i < k; i++) {
+            cout << A[i] << endl;
+        }
+    }
+    else {
+        for (int i = 0; i < k; i++) {
+            cout << B[i] << endl;
+        }
+    }
+
+    return 0;
+    system("pause");
+}
---
+++ Tasks/lab4/Task2.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <set>
+using namespace std;
+
+int main() {
+    set<int> A;
+    set<int> B;
+    int n = 0;
+    int element = 0;
+
+    cout << "Enter amount of elements for A: " << endl;
+    cin >> n;
+    cout << "Enter the elements for set A: " << endl;
+    for (int i = 0; i < n; i++) {
+        cin >> element;
+        A.insert(element);
+    }
+
+    cout << "Enter amount of elements for B: " << endl;
+    cin >> n;
+    cout << "Enter the elements for set B: " << endl;
+    for (int i = 0; i < n; i++) {
+        cin >> element;
+        B.insert(element);
+    }
+    int countA = A.size();
+    int countB = B.size();
+
+    if (countA < countB) {
+        for (int elA : A) { // берём все элементы A
+            cout << elA << endl;
+        }
+        cout << endl;
+    }
+    else if (countB < countA) {
+        for (int elB : B) { // берём все элементы B
+            cout << elB << endl;
+        }
+    }
+    else {
+        cout << "Sets have the same amount of elements" << endl;
+    }
+    system("pause");
+    return 0;
+}
